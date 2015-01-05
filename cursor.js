02a4bc839d497f6ca36cefa5d623aef6f66dc240
Cindy <cindyofwu@yahoo.com>
2015-01-05 11:42:07 -0500

    shapes

--- cursor.js
+++ cursor.js
@@ -1,4 +1,21 @@
 
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var shape = function Shape(x, y, w, h, fill) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.w = w || 1;
+    this.h = h || 1;
+    this.fill = fill || '#AAAAAA';
+}
+
+Shape.prototype.draw = function(ctx) {
+    ctx.fillStyle = this.fill;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+}
+
+
 $(document).ready(start);
 
 var start = function(){
